e20c8ed50c3b22028e11a8f6d8110257651337ad
Christian <c-mueller87@live.de>
2022-01-14 21:18:13 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+      this.items.push(item);
+      this.items.sort((a, b) => { return a - b });
+      this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos < this.items.length) { return this.items[pos]; }
+    if(pos > this.items.length) { throw new Error('OutOfBounds'); }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length != 0) { return Math.max(this.items); }
+    else { throw new Error('EmptySortedList'); }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length != 0) { return Math.min(this.items); }
+    else { throw new Error('EmptySortedList'); }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    for (let i = this.items.length; i--;) { sum += this.items[i]; }
+    return sum;
+  }
 
-  avg() {}
+  avg() { 
+    if(!this.items || this.items.length == 0) { throw new Error('EmptySortedList')}
+    else { this.sum() / this.items.length; }
+  }
 }
 
 module.exports = SortedList;
